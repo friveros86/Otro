9b9dd438a754e988430064cdf300d6a4410c9e78
LENOVO <LENOVO@User>
2018-06-17 11:12:44 -0500

    se crea funcionalidad de us feture 02

--- Otro2Project/src/com/FirstClass.java
+++ Otro2Project/src/com/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
 		
 		System.out.println("se agrega funcionalidad");
 		System.out.println("se agrega funcionalidad a dev");
+		System.out.println("comienzo a trabajar en un us a partir de la rama dev ");
 	}
 
 }
